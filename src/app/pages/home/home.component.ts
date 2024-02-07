4428e6dfe24e83270918c46fa411c6676e14006a
Carlo Mirador Jr <CEM.code404@gmail.com>
2024-02-07 13:36:24 +0800

    feat: put homepage initial layout

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from '../../shared/nav-bar/nav-bar.component';
 import { HeroComponent } from './hero/hero.component';
+import { MovieCardComponent } from '../../shared/movie-card/movie-card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavBarComponent, HeroComponent],
+  imports: [NavBarComponent, HeroComponent, MovieCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
