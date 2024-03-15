886de8d60ef15bfe9a2b9ea0a0cd3ad89415559e
Carlo Mirador Jr <CEM.code404@gmail.com>
2024-03-15 14:04:33 +0800

    fix: searchTitleGuard undefined error

--- src/app/pages/search-titles/guard/search-titles.guard.ts
+++ src/app/pages/search-titles/guard/search-titles.guard.ts
@@ -2,7 +2,7 @@ import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
 
 export const searchTitlesGuard: CanActivateFn = (route, state) => {
-  const movieTitle = route.queryParams['searchTitle'].trim();
+  const movieTitle = route.queryParams['searchTitle']?.trim();
 
   if (movieTitle && movieTitle.length > 0) return true;
   return inject(Router).createUrlTree(['']);
